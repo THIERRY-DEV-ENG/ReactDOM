25f01712bcd2eb7df7b2b7291d82a213b57eb21d
THIERRY-DEV-ENG <thierrynishime936@gmail.com>
2025-12-11 17:01:32 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+ const list = React.createElement(
+                                          "ul",
+                                          null,
+                                          React.createElement("li", null, "1"),
+                                          React.createElement("li", null, "2"),
+                                          React.createElement("li", null, "3"),
+                                          React.createElement("li", null, "4"),
+                                          React.createElement("li", null, "5"),
+                                          React.createElement("li", null, "6 ")
+                                );
+                                  console.log(list);
